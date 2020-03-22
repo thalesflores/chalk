00b18e05a9db1341abc00de2508f2b2ec9ab5756
thalesflores <thalesvflores@gmail.com>
2020-03-22 21:41:57 +0200

    add query formatter

--- lib/chalk.ex
+++ lib/chalk.ex
@@ -2,4 +2,42 @@ defmodule Chalk do
   @moduledoc """
   Documentation for `Chalk`.
   """
+
+  @spec query(params :: keyword()) :: String.t()
+  def query(params) do
+    params
+    |> to_graphql()
+    |> add_curly_braces()
+  end
+
+  defp to_graphql(query) when is_list(query) do
+    Enum.reduce(query, "", fn {action, fields}, acc ->
+      acc <> ~s(#{to_camel_case(action)}#{to_graphql_fields(fields)})
+    end)
+  end
+
+  defp to_graphql(query) when is_tuple(query), do: to_graphql([query])
+  defp to_graphql(field) when is_atom(field), do: to_camel_case(field)
+
+  defp to_graphql_fields(query) when is_list(query) do
+    for(field <- query, into: "", do: "#{to_graphql(field)} ")
+    |> String.trim()
+    |> add_curly_braces()
+  end
+
+  defp to_camel_case(key) do
+    [non_capitalize_word | to_capitalize_words] =
+      key
+      |> to_string()
+      |> String.split("_")
+
+    [non_capitalize_word, Enum.map(to_capitalize_words, &String.capitalize(&1))]
+    |> Enum.join("")
+  end
+
+  defp add_curly_braces(string) do
+    string
+    |> String.replace_prefix("", "{")
+    |> String.replace_suffix("", "}")
+  end
 end
--- test/chalk_test.exs
+++ test/chalk_test.exs
@@ -1,8 +1,19 @@
 defmodule ChalkTest do
   use ExUnit.Case
-  doctest Chalk
 
-  test "greets the world" do
-    assert Chalk.hello() == :world
+  describe "query/1" do
+    test "when passed a simple query and returns in graphql format" do
+      params = [get_users: [:name, :age]]
+      expected_response = "{getUsers{name age}}"
+
+      assert expected_response == Chalk.query(params)
+    end
+
+    test "when passed a query with subquery and returns in graphql format" do
+      params = [get_users: [:name, :age, posts: [:another_data]], visits: [:total]]
+      expected_response = "{getUsers{name age posts{anotherData}}visits{total}}"
+
+      assert expected_response == Chalk.query(params)
+    end
   end
 end
