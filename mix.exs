0f2e22bfaa243b5d7f74ec38f5536b7dae5f3fba
thalesflores <thalesvflores@gmail.com>
2020-04-28 20:28:26 +0300

    adjust mix with doc and CI libs

--- mix.exs
+++ mix.exs
@@ -6,23 +6,60 @@ defmodule Chalk.MixProject do
       app: :chalk,
       version: "0.1.0",
       elixir: "~> 1.10",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Chalk",
+      homepage_url: "https://github.com/thalesflores/chalk",
+      docs: [
+        source_url: "https://github.com/thalesflores/chalk",
+        extras: ["README.md"]
+      ],
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      elixirc_paths: elixirc_paths(Mix.env()),
+      name: "CPF",
+      dialyzer: [
+        plt_file: {:no_warn, "plts/dialyzer.plt"},
+        plt_add_apps: [:mix, :ex_unit]
+      ],
+      preferred_cli_env: [dialyzer: :test]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:httpoison, "~> 1.6"},
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.21", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: [:dev, :test], runtime: false},
+      {:bypass, "~> 1.0", only: :test},
+      {:plug_cowboy, "~> 2.0", only: :test}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  def description do
+    "A light GraphQL client to Elixir projects."
+  end
+
+  def package do
+    [
+      name: "Chalk",
+      maintainers: ["Thales Flores"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/thalesflores/chalk"}
     ]
   end
 end
